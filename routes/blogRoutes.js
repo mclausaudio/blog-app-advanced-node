e037d44d10ea181cf94b05d9631660517c7c592f
Michael Claus <michaelclaus@Postmans-MacBook-Pro-2.local>
2020-01-02 17:47:07 -0800

    add redis

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,28 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+    const redis = require('redis');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require('util');
 
+    // Do we have any cached data in redis related to this?
+    // Out of the box, Redis doesn't support promises and only supports callback pattern
+    // Can use NodeJS's util library to promisify client.get
+    client.get = util.promisify(client.get);
+    // We are overriding the old redis function with our promisified version so we can use async await
+    const cachedBlogs = await client.get(req.user.id);
+    // if yes, lets return redis cached data
+    if(cachedBlogs){
+      console.log('Serving from cache!')
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    // if no, respond to request and cache data
+    const blogs = await Blog.find({ _user: req.user.id });
+    console.log('Serving from MongoDB!')
     res.send(blogs);
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
